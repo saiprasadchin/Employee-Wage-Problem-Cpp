acf669d9d824a1c672d30f825a3015cf560a997f
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-16 23:11:08 -0700

    Added : Logic for checking attendance of employee

--- EmployeeWageProblem.cpp
+++ EmployeeWageProblem.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
+int checkAttendance();
 int main() {
-    cout << "------------------------------------";
-    cout << "\n Welcome to Employee Wage Problem" << endl;
+    int isPresent = 1;
+    int isAbsent = 2;
+    cout << "\n\n------------------------------------\n";
+    cout << "\nWelcome to Employee Wage Problem\n" << endl;
+    int attendance = checkAttendance();
+    if (attendance == isPresent) 
+        cout << "Employee is present" << endl;
+    else 
+        cout << "Employee is absent" << endl;
+    return 0;
+}
+
+int checkAttendance(){
+    srand(time(NULL));
+	return ((rand() % 2) + 1);
 } 
