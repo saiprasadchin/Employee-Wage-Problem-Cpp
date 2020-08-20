7ce50f8be2267d238de983387d69ebfa476f7ac1
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-20 09:42:58 -0700

    Added : Condition for 20 days or 100 hours

--- EmployeeWageProblem.cpp
+++ EmployeeWageProblem.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <time.h>
+#include <vector>
 using namespace std;
 int checkAttendance();
 int wageCalculator(int);
 int wagePerHour = 20;
-int totalWorkingDays = 20;
+const int totalWorkingDays = 20;
+const int totalWorkingHours = 100;
 
 int main()
 {
     srand(time(NULL));
     int fullDayHour = 8;
     int partDayHour = 4;
+    int noWorkDay = 0;
     int isPresent = 1;
     int isAbsent = 2;
     int isPartTime = 1;
     int isFullTime = 2;
     int totalWorkingDays = 20;
     int employeeWage;
+    vector<int> employeeMonthlyWage;
     cout << "\n\n\n------------------------------------\n\n";
-    cout << "\nWelcome to Employee Wage Problem\n" << endl;
-    for (int days = 0; days < totalWorkingDays; days++)
+    cout << "\nWelcome to Employee Wage Problem\n"
+         << endl;
+    int workedDays = 0;
+    int workedHours = 0;
+    while (workedDays < totalWorkingDays && workedHours < totalWorkingHours)
     {
         int decideAttendance = ((rand() % 2) + 1);
         int decideTypeHours = ((rand() % 2) + 1);
@@ -30,18 +37,31 @@ int main()
             {
                 cout << "Part Time wage\n";
                 employeeWage = wageCalculator(partDayHour);
+                workedHours += partDayHour;
             }
             else
             {
                 cout << "Full Time wage\n";
                 employeeWage = wageCalculator(fullDayHour);
+                workedHours += fullDayHour;
             }
             cout << "Employees wage for today : " << employeeWage << "\n"
                  << endl;
         }
         else
+        {
+            employeeWage = wageCalculator(noWorkDay);
             cout << "Employee is absent\n"
                  << endl;
+        }
+        employeeMonthlyWage.push_back(employeeWage);
+        workedDays++;
+    }
+    int days = 0;
+    for (int &wages : employeeMonthlyWage)
+    {
+        days++;
+        std::cout << "Day " << days << " - Wage : " << wages << "\n";
     }
     return 0;
 }
